402e8a0010323abc0f8597b4733d96e4538465d7
Venus <venuslam2003@outlook.com>
2019-12-17 09:58:56 +0100

    database

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=;Uid=;Pwd=;";
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=;Uid=110298;Pwd=rUartHOr;";
 
     public IActionResult Index()
     {
