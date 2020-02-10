6a7b8e79b6042fba5a149ed3fc9f458130c06a72
Venus <venuslam2003@outlook.com>
2020-02-10 15:29:54 +0100

    Pagina zakelijk gemaakt

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -57,7 +57,11 @@ namespace SchoolTemplate.Controllers
       return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult zakelijk()
+    {
+      return View();
+    }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
